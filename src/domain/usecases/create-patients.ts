1aa930b5e7cdf691fee4fd4290b6a0cd83c9737a
alextavella <eu.alextavella@gmail.com>
2021-01-07 19:35:22 -0300

    add create patient usecase

---
+++ src/domain/usecases/create-patients.ts
@@ -0,0 +1,14 @@
+import { Patient } from '@/domain/models'
+
+export interface CreatePatients {
+  create(model: CreatePatients.Model): Promise<CreatePatients.Response>
+}
+
+export namespace CreatePatients {
+  export type Model = {
+    name: string
+    cpf: string
+  }
+
+  export type Response = Patient
+}
