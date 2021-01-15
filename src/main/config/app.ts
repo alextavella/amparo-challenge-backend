196c693cab21fe9dcfeaacb9a620a51e4eb76982
alextavella <eu.alextavella@gmail.com>
2021-01-15 11:23:03 -0300

    config cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import { setupRoutes } from '@/main/config/routes'
+import cors from 'cors'
 import express from 'express'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 setupRoutes(app)
